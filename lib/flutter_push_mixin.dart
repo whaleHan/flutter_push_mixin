04609d829200478e67e0f7a3ea417bcaaf077d6f
caihan <923530524cai>
2021-11-30 13:58:24 +0800

    处理消息获取

--- lib/flutter_push_mixin.dart
+++ lib/flutter_push_mixin.dart
@@ -2,35 +2,57 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
 
 class FlutterPushMixin {
-  static const MethodChannel _methodChannel = MethodChannel('ss');
-
   static const EventChannel _channel = EventChannel('flutter_push_mixin');
 
   static const String _init = 'init_channel_push_mix';
 
-  static Stream<PushModel> initListener() async* {
+  static Logger log = Logger();
 
+  static Stream<PushModel> initListener({
+    required Function(RegIdModel model) getId,
+    required Function(PushModel model) getMessage,
+  }) async* {
     final Stream _stream = _channel.receiveBroadcastStream(_init);
 
-
-
     _stream.listen((event) {
-      print('监听回报数据: $event');
+      log.d('flutter_push_mixin -> : $event');
 
-      if(event == 'ok') {
-        //TODO: 初始化完成
-      } else if(event is Map) {
-        if(event['regId'] != null) {
-          //成功获取数据
-          print('获取regId成功');
+      if (event == 'ok') {
+
+        log.d('flutter_push_mixin -> : 初始化完成');
+
+      } else if (event is Map) {
+        if (event['regId'] != null) {
+          log.d('flutter_push_mixin -> : 成功获取regId');
+
+          RegIdModel _model = RegIdModel.fromJson(event as Map<String, dynamic>);
+
+          getId(_model);
         } else {
+          log.d('flutter_push_mixin -> : 成功获取regId');
+
           PushModel _model = PushModel.fromJson(event as Map<String, dynamic>);
+
+          getMessage(_model);
         }
       }
     });
   }
+}
+
+class RegIdModel {
+  final String? platformName;
+  final String? regId;
+
+  const RegIdModel({required this.platformName, required this.regId});
+
+  factory RegIdModel.fromJson(Map<String, dynamic> json) => RegIdModel(
+        platformName: json['platformName'],
+        regId: json['regId'],
+      );
 }
 
 class PushModel {
