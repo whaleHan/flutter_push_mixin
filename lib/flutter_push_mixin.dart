2113ac87a8461ead359c3bdce830286ebc40a91f
ch <7678365+cai_han@user.noreply.gitee.com>
2022-04-26 12:09:27 +0800

    添加Apns获取，添加权限获取

--- lib/flutter_push_mixin.dart
+++ lib/flutter_push_mixin.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_apns_only/flutter_apns_only.dart';
 import 'package:logger/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FlutterPushMixin {
-  static const BasicMessageChannel _channel = BasicMessageChannel('flutter_push_mixin', StringCodec());
+  static const BasicMessageChannel _channel = BasicMessageChannel(
+      'flutter_push_mixin', StringCodec());
 
   static const String _init = 'init_channel_push_mix';
 
@@ -14,34 +19,54 @@ class FlutterPushMixin {
   static Stream<PushModel> initListener({
     required Function(RegIdModel model) getId,
     required Function(PushModel model) getMessage,
+    required Function(String apnsToken) getApnsToken,
   }) async* {
-
-    _channel.setMessageHandler((message) async{
-      log.d('flutter_push_mixin -> : $message');
-
-      if (message == 'ok') {
-
-        log.d('flutter_push_mixin -> : 初始化完成');
-
-      } else if (message is Map) {
-        if (message['regId'] != null) {
-          log.d('flutter_push_mixin -> : 成功获取regId');
-
-          RegIdModel _model = RegIdModel.fromJson(message as Map<String, dynamic>);
-
-          getId(_model);
-        } else {
-          log.d('flutter_push_mixin -> : 获取到消息');
-
-          PushModel _model = PushModel.fromJson(message as Map<String, dynamic>);
-
-          getMessage(_model);
+    await Permission.notification.request();
+    if (Platform.isIOS) {
+      final ApnsPushConnectorOnly connector = ApnsPushConnectorOnly();
+      connector.configureApns(
+          onMessage: (ApnsRemoteMessage message) async{
+            final PushModel _model = PushModel(
+              payload: jsonEncode(message.payload),
+            );
+            getMessage(_model);
+          }
+      );
+      connector.token.addListener(() {
+        final String? _token = connector.token.value;
+        if(_token != null) {
+          getApnsToken(_token);
         }
-      }
-    });
+      });
+      connector.requestNotificationPermissions();
+    } else {
+      _channel.setMessageHandler((message) async {
 
-    _channel.send(_init);
+        log.d('flutter_push_mixin -> : $message');
 
+        if (message == 'ok') {
+          log.d('flutter_push_mixin -> : 初始化完成');
+        } else if (message is Map) {
+          if (message['regId'] != null) {
+            log.d('flutter_push_mixin -> : 成功获取regId');
+
+            RegIdModel _model = RegIdModel.fromJson(
+                message as Map<String, dynamic>);
+
+            getId(_model);
+          } else {
+            log.d('flutter_push_mixin -> : 获取到消息');
+
+            PushModel _model = PushModel.fromJson(
+                message as Map<String, dynamic>);
+
+            getMessage(_model);
+          }
+        }
+      });
+
+      _channel.send(_init);
+    }
   }
 }
 
@@ -51,7 +76,8 @@ class RegIdModel {
 
   const RegIdModel({required this.platformName, required this.regId});
 
-  factory RegIdModel.fromJson(Map<String, dynamic> json) => RegIdModel(
+  factory RegIdModel.fromJson(Map<String, dynamic> json) =>
+      RegIdModel(
         platformName: json['platformName'],
         regId: json['regId'],
       );
@@ -64,14 +90,15 @@ class PushModel {
   final String? payload;
   final bool? passThrough;
 
-  const PushModel(
-      {required this.description,
-      required this.payload,
-      required this.title,
-      required this.platform,
-      required this.passThrough});
+  const PushModel({
+    this.payload,
+    this.description,
+    this.title,
+    this.platform,
+    this.passThrough});
 
-  factory PushModel.fromJson(Map<String, dynamic> json) => PushModel(
+  factory PushModel.fromJson(Map<String, dynamic> json) =>
+      PushModel(
         payload: json['payload'],
         platform: json['platform'],
         title: json['title'],
